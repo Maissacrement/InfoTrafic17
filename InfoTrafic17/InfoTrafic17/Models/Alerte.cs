9506b8eb1b89a9d2f85d668bb264b6908f462635
Pierre <pierre.cesmat@reseau.eseo.fr>
2018-06-10 18:08:12 +0200

    creation projet

---
+++ InfoTrafic17/InfoTrafic17/Models/Alerte.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace InfoTrafic17.Models
+{
+    public class Alerte
+    {
+        public string Id { get; set; }
+        public string Titre { get; set; }
+        public string Description { get; set; }
+        public int Niveau { get; set; }
+        public DateTime DatePost { get; set; }
+        public Color Color { get; set; }
+
+        public Alerte()
+        {
+            CreateAlerte("Test", "Ceci est un test", 3, new DateTime(2018, 6, 10));
+        }
+
+        public Alerte(String titre, String description, Int32 niveau, DateTime date)
+        {
+            CreateAlerte(titre, description, niveau, date);
+        }
+
+        private void CreateAlerte(String titre, String description, Int32 niveau, DateTime date)
+        {
+            Titre = titre;
+            Description = description;
+            Niveau = niveau;
+            DatePost = date;
+
+            if (niveau == 1)
+            {
+                Color = Color.LightGray;
+            }
+            else if (niveau == 2)
+            {
+                Color = Color.LightBlue;
+            }
+            else if (niveau == 3)
+            {
+                Color = Color.Yellow;
+            }
+            else if (niveau == 4)
+            {
+                Color = Color.Orange;
+            }
+            else if (niveau == 5)
+            {
+                Color = Color.Red;
+            }
+        }
+
+
+        public Int32 Heures
+        {
+            get
+            {
+                return (DateTime.Now - DatePost).Hours;
+            }
+        }
+    }
+}
---
+++ InfoTrafic17/InfoTrafic17/Views/AlertesPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using InfoTrafic17.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace InfoTrafic17.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AlertesPage : ContentPage
+	{
+		public AlertesPage ()
+		{
+			InitializeComponent ();
+
+            alertes.ItemTapped += (object sender, ItemTappedEventArgs e) => {
+                // don't do anything if we just de-selected the row
+                if (e.Item == null) return;
+                // do something with e.SelectedItem
+                ((ListView)sender).SelectedItem = null;
+            };
+
+
+
+            alertes.ItemsSource = new List<Alerte>() {
+                new Alerte("⚠ 🐶 Chatel - Angoulin" , "Attention chien érrant sur la D925 entre angoulins et chatelaillon", 3, new DateTime(2018,6,10,5,15,00)),
+                new Alerte("🚗 💥 Ile de ré" , "Accident sur la départementale entre la flotte et sainte-marie", 2, new DateTime(2018,6,10,8,15,00)),
+                new Alerte("🚓 5️0️ Boulevard sautel" , "Police boulevard sautel au niveau du Leclerc", 1, new DateTime(2018,6,10,12,15,00)),
+                new Alerte("🔥 🚒 Muron" , "Voiture en feu dans le centre ville de muron, route totalement bloquée ! Pompier sur place", 5, new DateTime(2018,6,10,12,15,00)),
+                new Alerte("⚠ 🐶 Chatel - Angoulin" , "Attention chien érrant sur la D925 entre angoulins et chatelaillon", 3, new DateTime(2018,6,10,5,15,00)),
+                new Alerte("🚗 💥 Ile de ré" , "Accident sur la départementale entre la flotte et sainte-marie", 4, new DateTime(2018,6,10,8,15,00)),
+                new Alerte("🚓 5️0️ Boulevard sautel" , "Police boulevard sautel au niveau du Leclerc", 2, new DateTime(2018,6,10,12,15,00)),
+                new Alerte("🔥 🚒 Muron" , "Voiture en feu dans le centre ville de muron, route totalement bloquée ! Pompier sur place", 5, new DateTime(2018,6,10,12,15,00))
+            };
+        }
+	}
+}
